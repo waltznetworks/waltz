8d5a9661c6ef4f32b620bff43bc7cb0920bec62e
Archit Baweja <architbaweja@gmail.com>
2015-03-10 18:21:11 -0700

    Update contact.php to register user in the app database.

--- waltzwebsite/contact.php
+++ waltzwebsite/contact.php
@@ -12,10 +12,37 @@ $body_message .= 'Message: '.$field_message."\n";
 $headers = 'From: '.$field_email."\r\n";
 $headers .= 'Reply-To: '.$field_email."\r\n";
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
+$curl = curl_init("https://app.waltznetworks.com/register");
 
+$registeration_data = array(
+    "name" => $field_name,
+    "email" => $field_email,
+    "registrationMessage" => $field_message
+);
+
+foreach ($registeration_data as $key => $key_value) {
+  $query_data[] = urlencode($key) . '=' . urlencode($key_value);
+}
+$query_data = implode('&', $query_data);
+
+curl_setopt_array($curl, array(
+    CURLOPT_CONNECTTIMEOUT => 30,
+    CURLOPT_USERAGENT      => "www.waltznetworks.com contact.php",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_POST           => 1,
+    CURLOPT_POSTFIELDS     => $query_data,
+    CURLOPT_HTTPHEADER     => array('Content-Type: application/x-www-form-urlencoded')
+));
+curl_exec($curl);
+curl_close($curl);
+
+$body_message .= 'POST-Data: ' . $query_data;
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) {
-  header('Location: /#contact');
+    header('Location: /#contact');
 } else {
 ?>
 	<type="text/javascript">
